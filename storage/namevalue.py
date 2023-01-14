037135cc48e04af973ec6a17712647316240a944
willdrug <whydafuqdoineedthis@gmail.com>
2023-01-14 14:28:30 +0100

    .

--- storage/namevalue.py
+++ storage/namevalue.py
@@ -1,29 +1,28 @@
 class DomainNameValue:
     def __init__(self, domain, collection):
-        self.domain = domain
-        self.__collection = collection
+        object.__setattr__(self, 'domain', domain)
+        object.__setattr__(self, 'collection', collection)
+
 
     def _get(self, item):
-        d = self.__collection.find_one({"domain": self.domain, "key": item})
+        d = object.__getattribute__(self, 'collection').find_one({"domain": object.__getattribute__(self, 'domain'),
+                                                                  "key": item})
         if d is None:
             return None
         else:
             return d["value"]
 
     def _set(self, key, value):
-        return self.__collection.update_one({"name": key, "domain": self.domain}, {"$set": {"domain": self.domain,
-                                                                                            "name": key,
-                                                                                            "value": value}},
-                                            upsert=True)
+        return object.__getattribute__(self, 'collection').\
+            update_one({"name": key, "domain": object.__getattribute__(self, 'domain')},
+                       {"$set": {"domain": object.__getattribute__(self, 'domain'),
+                                 "name": key,"value": value}},
+                       upsert=True)
 
     def __getattr__(self, item):
-        if item in ['domain', '_DomainNameValue__collection']:  # fixme: figure out a way around this hack
-            return object.__getattribute__(self, item)
         return self._get(item)
 
     def __setattr__(self, key, value):
-        if key in ['domain', '_DomainNameValue__collection']:
-            return object.__setattr__(self, key, value)
         return self._set(key, value)
 
     def __setitem__(self, key, value):
