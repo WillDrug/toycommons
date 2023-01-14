3388583f10462183f437bfd8221c50e00ce746a6
willdrug <whydafuqdoineedthis@gmail.com>
2023-01-14 14:23:30 +0100

    .

--- storage/namevalue.py
+++ storage/namevalue.py
@@ -5,23 +5,27 @@
 class DomainNameValue:
     def __init__(self, domain, collection):
         self.domain = domain
-        self._collection = collection
+        self.__collection = collection
 
     def _get(self, item):
-        d = self._collection.cache.find_one({"domain": self.domain, "key": item})
+        d = self.__collection.find_one({"domain": self.domain, "key": item})
         if d is None:
             return None
         else:
             return d["value"]
 
     def _set(self, key, value):
-        return self._collection.update_one({"name": key, "domain": self.domain}, {"domain": self.domain,
+        return self.__collection.update_one({"name": key, "domain": self.domain}, {"domain": self.domain,
                                                                                    "name": key,
                                                                                    "value": value}, upsert=True)
     def __getattr__(self, item):
+        if item in ['domain', '_DomainNameValue__collection']:  # fixme: figure out a way around this hack
+            return object.__getattribute__(self, item)
         return self._get(item)
 
     def __setattr__(self, key, value):
+        if key in ['domain', '_DomainNameValue__collection']:
+            return object.__setattr__(self, key, value)
         return self._set(key, value)
 
     def __setitem__(self, key, value):
@@ -29,3 +33,4 @@ class DomainNameValue:
 
     def __getitem__(self, item):
         return self._get(item)
+
