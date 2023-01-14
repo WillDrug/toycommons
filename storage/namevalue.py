f9c4b24e1b19bb5e5bd368a9839653e293123699
willdrug <whydafuqdoineedthis@gmail.com>
2023-01-14 14:19:14 +0100

    .

--- storage/namevalue.py
+++ storage/namevalue.py
@@ -5,17 +5,17 @@
 class DomainNameValue:
     def __init__(self, domain, collection):
         self.domain = domain
-        self.__collection = collection
+        self._collection = collection
 
     def _get(self, item):
-        d = self.__collection.cache.find_one({"domain": self.domain, "key": item})
+        d = self._collection.cache.find_one({"domain": self.domain, "key": item})
         if d is None:
             return None
         else:
             return d["value"]
 
     def _set(self, key, value):
-        return self.__collection.update_one({"name": key, "domain": self.domain}, {"domain": self.domain,
+        return self._collection.update_one({"name": key, "domain": self.domain}, {"domain": self.domain,
                                                                                    "name": key,
                                                                                    "value": value}, upsert=True)
     def __getattr__(self, item):
