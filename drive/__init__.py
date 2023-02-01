2e1a2b282237b4e704225f9abb6de962b00ba505
willdrug <whydafuqdoienedthis@gmail.com>
2023-02-01 09:32:23 +0100

    ddd

--- drive/__init__.py
+++ drive/__init__.py
@@ -33,6 +33,7 @@ class Directory:
         self.__config = config
         self.__sync_field = sync_config_field
         self.__cache_db = cache
+        self.__cache_db[f'{self.name}_last_cached'] = 0  # reinit relist.
         self.__service = service
 
     @property
